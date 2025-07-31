698a52138df61965a0146ad7bc88a7423c5a1ba8
jordan h <jharrison1@qa.com>
2025-07-31 15:38:43 +0100

    fixed problem with words with odd lengths

--- TDD-Demo-4/src/main/java/com/s2s/palindrome/Palindrome.java
+++ TDD-Demo-4/src/main/java/com/s2s/palindrome/Palindrome.java
@@ -1,49 +1,21 @@
 package com.s2s.palindrome;
 
 public class Palindrome {
-    private class Result {
-        private String inputString;
-        private int count = 0;
 
-        public Result(String inputString, int initCount) {
-            this.inputString = inputString;
-            count = initCount;
-        }
-
-        public Result(Result inputValues) {
-            this.count = inputValues.count;
-            this.inputString = inputValues.inputString;
-        }
-    }
-
-    public int isPalindrome(String inputString) {
+    public static int isPalindrome(String inputString) {
         int result = 0;
+        if (!(inputString == null || inputString.isEmpty())) {
+            char[] letters = inputString.toCharArray();
 
-        if (inputString.length() == 0) {
-            result = 1;
-        } else {
-            char firstChar = inputString.charAt(0);
-            char lastChar = inputString.charAt(inputString.length() - 1);
-            String mid = inputString.substring(1, inputString.length() - 1);
-            if (firstChar == lastChar) {
-                Result tempResult = new Result(mid, 1);
-                isPalindrome(tempResult);
-                result = tempResult.count;
+            for (int i = 0; i < letters.length / 2; i++) {
+                if (letters[i] == letters[letters.length - (i + 1)]) result++;
+                else break;
             }
+
         }
+
         return result;
     }
 
-    private void isPalindrome(Result inputValues) {
-        if (inputValues.inputString.length() > 0) {
-            char firstChar = inputValues.inputString.charAt(0);
-            char lastChar = inputValues.inputString.charAt(inputValues.inputString.length() - 1);
-            String mid = inputValues.inputString.substring(1, inputValues.inputString.length() - 1);
-            if (firstChar == lastChar) {
-                inputValues.count++;
-                inputValues.inputString = mid;
-                isPalindrome(inputValues);
-            }
-        }
-    }
+
 }
--- TDD-Demo-4/src/test/java/com/s2s/palindrome/PalindromeTest.java
+++ TDD-Demo-4/src/test/java/com/s2s/palindrome/PalindromeTest.java
@@ -5,31 +5,34 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PalindromeTest {
+
     @Test
-    public void when_imputLength_is_zero_then_accept() {
-        Palindrome palindromeTester = new Palindrome();
-        //assertTrue(palindromeTester.isPalindrome(""));
-        assertEquals(1, palindromeTester.isPalindrome(""));
+    public void whenInputIsNullReturnZero() {
+        assertEquals(0, Palindrome.isPalindrome(""));
     }
 
     @Test
-    public void when_palindrome_of_two_letters_then_accept() {
-        Palindrome palindromeTester = new Palindrome();
-//        assertTrue(palindromeTester.isPalindrome("noon"));
-        assertEquals(1, palindromeTester.isPalindrome("oo"));
+    public void whenInputLengthIsZeroReturnZero() {
+        assertEquals(0, Palindrome.isPalindrome(""));
     }
 
     @Test
-    public void when_palindrome_of_four_letters_then_accept() {
-        Palindrome palindromeTester = new Palindrome();
-//        assertTrue(palindromeTester.isPalindrome("noon"));
-        assertEquals(2, palindromeTester.isPalindrome("noon"));
+    public void whenPalindromeOfTwoLettersThenAccept() {
+        assertEquals(1, Palindrome.isPalindrome("oo"));
     }
 
     @Test
-    public void when_palindrome_of_six_letters_then_accept() {
-        Palindrome palindromeTester = new Palindrome();
-//        assertTrue(palindromeTester.isPalindrome("noon"));
-        assertEquals(3, palindromeTester.isPalindrome("snoons"));
+    public void whenPalindromeOfFourLettersThenAccept() {
+        assertEquals(2, Palindrome.isPalindrome("noon"));
+    }
+
+    @Test
+    public void whenPalindromeOfSevenLettersThenAccept() {
+        assertEquals(3, Palindrome.isPalindrome("racecar"));
+    }
+
+    @Test
+    public void fail() {
+        assertEquals(0, Palindrome.isPalindrome("gdfgdgd"));
     }
 }
